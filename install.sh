5ca95b97cfb3c43b810b0f16e6c501719256fc86
19gabates <garrettbates1100@gmail.com>
2023-12-03 18:34:10 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
 
-# List of required commands
-required_commands=("awk" "sed" "nc" "curl")
+# Install necessary commands
+sudo apt-get update
+sudo apt-get install -y curl
 
-# Check if all required commands are installed
-missing_commands=()
-for cmd in "${required_commands[@]}"; do
-    if ! command -v "$cmd" &> /dev/null; then
-        missing_commands+=("$cmd")
-    fi
-done
-
-if [ ${#missing_commands[@]} -eq 0 ]; then
-    echo "All required commands are installed. Proceeding with the installation."
+# Check if curl is installed successfully
+if command -v curl &> /dev/null; then
+    echo "curl is installed successfully."
 else
-    # Install missing commands
-    echo "Installing missing commands: ${missing_commands[@]}"
-    
-    # Adjust the package manager based on your distribution (apt, yum, etc.)
-    # Uncomment and modify the following lines based on your package manager
-    # For example, on Debian/Ubuntu systems:
-    # sudo apt-get update
-    # sudo apt-get install "${missing_commands[@]}"
-    
-    # For example, on Red Hat/Fedora systems:
-    # sudo yum install "${missing_commands[@]}"
+    echo "Error: Failed to install curl."
+    exit 1
 fi
 
-# Make the scripts executable
-chmod +x scoring.sh case.sh view_dashboard.sh stat.sh
+# Create necessary files
+touch ip_addr_list.txt
+touch current_scores.txt
 
-echo "Installation completed."
+# Make scripts executable
+chmod +x scoring.sh
+chmod +x view_dashboard.sh
+chmod +x stat.sh
+
+# Display completion message
+echo "Installation completed successfully."
